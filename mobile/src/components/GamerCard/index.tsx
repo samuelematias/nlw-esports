dd3a61974d7fc6c875b3c74bb6b20af8f37f0edb
Samuel Matias <hi@samuelematias.com>
2022-09-17 19:04:32 -0300

    feat(mobile): implement get api/ create gamespage

--- mobile/src/components/GamerCard/index.tsx
+++ mobile/src/components/GamerCard/index.tsx
@@ -2,7 +2,6 @@ import {
     TouchableOpacity,
     TouchableOpacityProps,
     ImageBackground,
-    ImageSourcePropType,
     Text,
 } from 'react-native';
 
@@ -14,9 +13,11 @@ import { styles } from './styles';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number;
+    }
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -31,18 +32,18 @@ export function GameCard({ data, ...rest }: Props) {
         >
             <ImageBackground
                 style={styles.cover}
-                source={data.cover}
+                source={{ uri: data.bannerUrl }}
             >
                 <LinearGradient
                     colors={THEME.COLORS.FOOTER}
                     style={styles.footer}
                 >
                     <Text style={styles.name}>
-                        {data.name}
+                        {data.title}
                     </Text>
 
                     <Text style={styles.ads}>
-                        {data.ads} anúncios
+                        {data._count.ads} anúncios
                     </Text>
                 </LinearGradient>
 
